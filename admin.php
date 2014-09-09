3e5d7e5134ea48514233b6f39776001f11abc96d
Adam Toms <adamtoms@hotmail.co.uk>
2014-09-09 08:59:48 +0100

    Hash Password
    login now checks password against a hash. By no means secure, but better
    than plaintext?

--- admin.php
+++ admin.php
@@ -99,8 +99,7 @@ function login() {
   if ( isset( $_POST['login'] ) ) {
  
     // User has posted the login form: attempt to log the user in
- 
-    if ( $_POST['username'] == ADMIN_USERNAME && $_POST['password'] == ADMIN_PASSWORD ) {
+	if ( $_POST['username'] == ADMIN_USERNAME && $_POST['password'] AND sha1($_POST['password']) == ADMIN_PASSWORD ) {
  
       // Login successful: Create a session and redirect to the admin homepage
       $_SESSION['username'] = ADMIN_USERNAME;
